74e8931897a727e22286b7bdf45f17e1b2f37df2
Chedra Soor <csoor96@gmail.com>
2022-04-06 15:12:12 -0700

    Greetings

---
+++ Week1/Greetings/Test.java
@@ -0,0 +1,8 @@
+// The file name must be the same as the class name
+//Every class IS  a class
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
