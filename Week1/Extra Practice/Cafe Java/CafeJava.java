3d176aa7efb2652c7f6b5c41dae21b91110254c7
Chedra Soor <csoor96@gmail.com>
2022-04-10 09:56:37 -0700

    Cafe Java Practice

--- Week1/Extra Practice/Cafe Java/CafeJava.java
+++ Week1/Extra Practice/Cafe Java/CafeJava.java
@@ -28,6 +28,28 @@ public class CafeJava {
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
-        
+        System.out.println(customer1 + pendingMessage);
+
+        if (isReadyOrder4) 
+        {
+            System.out.println(customer4 + pendingMessage);
+        }
+        else 
+        {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage +  cappucino);
+        }
+
+        System.out.println(displayTotalMessage + latte * 2);
+        if (isReadyOrder2) 
+        {
+            System.out.println(customer2 + pendingMessage);
+        }
+        else
+        {
+            System.out.println(customer2 + readyMessage);
+        }
+
+        System.out.println(displayTotalMessage + (dripCoffee - latte));
     }
 }
