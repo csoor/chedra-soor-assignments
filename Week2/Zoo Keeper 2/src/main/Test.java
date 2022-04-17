faa99b4408db380f13cd9a59bae7b4d7c0d63720
Chedra Soor <csoor96@gmail.com>
2022-04-17 12:17:38 -0700

    ZooKeeper 2

---
+++ Week2/Zoo Keeper 2/src/main/Test.java
@@ -0,0 +1,19 @@
+package main;
+
+import species.Bat;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bat b = new Bat();
+		b.attackTown();
+		b.attackTown();
+		b.attackTown();
+		b.eatHumans();
+		b.eatHumans();
+		b.fly();
+		
+	}
+
+}
