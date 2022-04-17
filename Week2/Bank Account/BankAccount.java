c7c5d2ebe8270fb606acfe23599aac021b42a441
Chedra Soor <csoor96@gmail.com>
2022-04-17 09:36:17 -0700

    Bank Accocunt

---
+++ Week2/Bank Account/BankAccount.java
@@ -0,0 +1,69 @@
+public class BankAccount {
+    private double checking;
+    private double savings;
+    public static int numAccount = 0;
+    public static double total = 0;
+
+    //Constructor
+    public BankAccount(){
+        numAccount ++;
+        this.checking = 0;
+        this.savings = 0;
+    }
+    //Getters
+    public double getChecking()
+    {
+        return checking;
+    }
+    public double getSavings()
+    {
+        return savings;
+    }
+    public void deposit(double amount, String account)
+    {
+        if (account == "checking") 
+        {
+            checking += amount;
+        } 
+        else if(account == "savings")
+        {
+            savings += amount;
+        }
+
+    }
+
+    public void withdraw(double amount, String account)
+    {
+        boolean complete = false;
+        if (account == "checking")
+        {
+            if(checking - amount >= 0)
+            {
+                complete = true;
+                checking -= amount;
+            }
+        }
+        else if(account == "savings")
+        {
+            if(savings - amount >= 0)
+            {
+                complete = true;
+                savings -= amount;
+            }
+        }
+        if(complete)
+        {
+            total -= amount;
+        }
+        else 
+        {
+            System.out.println("Insufficient funds.");
+        }
+    }
+
+    public void displayBalance()
+    {
+        System.out.printf("Checking: %s\n", checking);
+        System.out.printf("Savings: %s\n", savings);
+    }
+}
---
+++ Week2/Bank Account/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args)
+    {
+        BankAccount myAccount = new BankAccount();
+
+        myAccount.deposit(500.50, "checking");
+        myAccount.deposit(100, "savings");
+
+        myAccount.displayBalance();
+
+        myAccount.withdraw(99, "savings");
+
+        myAccount.displayBalance();
+    }
+}
