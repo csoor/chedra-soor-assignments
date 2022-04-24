02c78731e36b884a972fa4e940b572adbf66b868
Chedra Soor <csoor96@gmail.com>
2022-04-24 13:41:33 -0700

    Fruity Markets

---
+++ Week3/Fruity Loops/fruitloops/src/main/java/com/codingdojo/models/Item.java
@@ -0,0 +1,12 @@
+package com.codingdojo.models;
+
+public class Item {
+	public String name;
+	public double price;
+	
+	public Item(String name, double price)
+	{
+		this.name = name;
+		this.price = price;
+	}
+}
